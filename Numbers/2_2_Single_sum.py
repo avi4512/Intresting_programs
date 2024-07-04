e06a2a414876860309046d98ce4a9f2744507652
avi4512 <144973797+avi4512@users.noreply.github.com>
2024-07-04 10:22:55 +0530

    Update 2_2_Single_sum.py

--- Numbers/2_2_Single_sum.py
+++ Numbers/2_2_Single_sum.py
@@ -1,13 +1,13 @@
 n = int(input("Enter the number:"))
 result = 0
-a_n = abs(n)
-n_s = str(a_n)
-while a_n >= 10:
+a_n = abs(n)   #convertinf negative to positive number
+n_s = str(a_n) #converting int to string
+while a_n > 9:
     for i in n_s:
         result = result + int(i)
-    a_n = result
-    n_s = str(result)
-    result = 0
+    a_n = result       #upating a_n as result fro next iteration 
+    n_s = str(result)  #again chaging the int to string
+    result = 0         #For next iterstion chaning result as 0
 if n < 0:
     main_ans = -a_n
 else:
